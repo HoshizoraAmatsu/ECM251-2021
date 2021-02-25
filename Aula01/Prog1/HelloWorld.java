e97b7bb0508139a148b684eba879a624fa80d0d7
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-02-25 09:12:51 -0300

    Adição de legendas para o terminal

--- Aula01/Prog1/HelloWorld.java
+++ Aula01/Prog1/HelloWorld.java
@@ -3,5 +3,12 @@ class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        /*
+         * No terminal:
+         *      "cd" - Set location
+         *      "ls" - Leitura de arquivos dentro de uma pasta
+         *      "javac" - Função para compilar um arquivo JAVA
+         *      "java" - Função para rodar um arquivo JAVA
+         */
     }
 }
