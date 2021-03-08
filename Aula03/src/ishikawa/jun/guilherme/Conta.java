1c881df919f4087a6d23dc6b6d0e17b331f0724e
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-08 08:26:33 -0300

    class Conta
    Criação da classe Conta (Conta bancária)
    Adição dos atributos e métodos para funcionar como conta bancária de pessoa física.

---
+++ Aula03/src/ishikawa/jun/guilherme/Conta.java
@@ -0,0 +1,16 @@
+package ishikawa.jun.guilherme;
+
+public class Conta
+{
+    //Atributos da classe
+    String titular;
+    String cpf;
+    double saldo;
+    int numero;
+
+    //Métodos da classe
+    void depositar() {}
+    void sacar() {}
+    void transferirDinheiro() {}
+    void visualizarSaldo() {}
+}
