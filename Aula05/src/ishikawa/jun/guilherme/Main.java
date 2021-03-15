d5d9c42c5ce9fa136cc7e5e8836a0ef18ff567c5
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-15 09:00:40 -0300

    Aula 05
    Aplicação de todos os conhecimentos das aulas passadas para a criação de um sistema MauaBank.
    
    No Sistema.java:
    Criação da operação Sacar e Visualizar saldo

---
+++ Aula05/src/ishikawa/jun/guilherme/Main.java
@@ -0,0 +1,10 @@
+package ishikawa.jun.guilherme;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Sistema sistema = new Sistema();
+        sistema.executar();
+    }
+}
---
+++ Aula05/src/ishikawa/jun/guilherme/Sistema.java
@@ -0,0 +1,65 @@
+package ishikawa.jun.guilherme;
+
+import java.util.Scanner;
+
+public class Sistema
+{
+    private boolean executarSistema;
+    private Scanner scanner;
+    private Conta conta;
+
+    public void executar()
+    {
+        int opcao;
+        while(executarSistema)
+        {
+            exibirMenu();
+            opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+        }
+    }
+
+    private void avaliarOpcao(int opcao)
+    {
+        switch (opcao)
+        {
+            case 0:
+                System.out.println("Obrigado por escolher o MauaBank!");
+                this.executarSistema = false;
+                break;
+            case 1:
+                System.out.println("Saldo atual: R$" + this.conta.getSaldo());
+                break;
+            case 2:
+                System.out.println("Qual a quantia de dinheiro?");
+                double quantiaParaSacar = scanner.nextDouble();
+                if (this.conta.sacar(quantiaParaSacar))
+                {
+                    System.out.println("Operação Realizada com Sucesso!");
+                } else {
+                    System.out.println("Não foi possível realizar a operação!");
+                }
+            default:
+                System.out.println("Essa funcionalidade ainda não foi implementada");
+        }
+    }
+
+    public Sistema()
+    {
+        this.scanner = new Scanner(System.in); //Deixa ele ler da entrada do sistema - teclado
+        this.executarSistema = true;
+        this.conta = new Conta("Mario", 1945, 1234);
+    }
+
+    private void exibirMenu()
+    {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Vizualizar Saldo");
+        System.out.println("2 - Sacar Dinheiro");
+        System.out.println("3 - Depositar Dinheiro");
+        System.out.println("4 - Transferir Dinheiro");
+        System.out.println("5 - Pagar Conta (Título)");
+        System.out.println("0 - Encerrar");
+    }
+}
+
