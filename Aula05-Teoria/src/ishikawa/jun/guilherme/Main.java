ae8eb6179c6ef2d90f9949871b8c3c80e37db6ea
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-18 09:14:22 -0300

    Aula05-Teoria
    Criação de classe e métodos para um validador de CPF

---
+++ Aula05-Teoria/src/ishikawa/jun/guilherme/Main.java
@@ -0,0 +1,10 @@
+package ishikawa.jun.guilherme;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String cpf = "05369466100";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
