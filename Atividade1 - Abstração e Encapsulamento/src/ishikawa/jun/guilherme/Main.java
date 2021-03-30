085fa78fe0abcf8054177578ab7121dd2e79b980
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-30 11:37:12 -0300

    Ajustes na main
    Agora o usuario insere o nome das contas, e mais alguns ajustes

--- Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
+++ Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
@@ -2,13 +2,24 @@
 
 package ishikawa.jun.guilherme;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
-	    Contas user1 = new Contas(1, 1000, "user1");
-        Contas user2 = new Contas(2, 250, "user2");
-        Contas user3 = new Contas(3, 3000, "user3");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o nome do Usuário 1: ");
+        String nome1 = scanner.nextLine();
+        System.out.println("Informe o nome do Usuário 2: ");
+        String nome2 = scanner.nextLine();
+        System.out.println("Informe o nome do Usuário 3: ");
+        String nome3 = scanner.nextLine();
+
+	    Contas user1 = new Contas(1, 1000, nome1);
+        Contas user2 = new Contas(2, 250, nome2);
+        Contas user3 = new Contas(3, 3000, nome3);
 
         Transacoes op = new Transacoes();
 
@@ -20,14 +31,14 @@ public class Main {
         String QR = op.gerarQR(250, user1);
         System.out.println(QR);
 
-        op.pagamento(QR, user2);
-        op.pagamento(QR, user3);
-        op.pagamento(QR, user2);
+        op.pagamento(QR, user2, user1);
+        op.pagamento(QR, user3, user1);
+        op.pagamento(QR, user2, user1);
 
         String QR2 = op.gerarQR(1000, user2);
         System.out.println(QR2);
 
-        op.pagamento(QR2, user2);
+        op.pagamento(QR2, user3, user2);
 
         System.out.println("Estado Final:");
         System.out.println("Nome Usuário: " + user1.getNome() + " - Saldo: " + user1.getSaldo());
