1f98e7a065a1bdd344f80c1f30eece33a3db0195
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-30 09:47:17 -0300

    Criação de métodos em Transacoes.java
    getRandomNumberInRange: Criação de um número aleatório para a geração de um QR
    
    gerarQR: Gera um QR usando o id da conta, nome do usuario, valor e codigo criado por getRandomNumberInRange
    
    pagamento: para realização de pagamentos.

--- Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Transacoes.java
+++ Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Transacoes.java
@@ -2,6 +2,26 @@
 
 package ishikawa.jun.guilherme;
 
+import java.util.Random;
+
 public class Transacoes
 {
+    private static int getRandomNumberInRange(int min ,int max)
+    {
+        Random r = new Random();
+        return r . nextInt((max - min) + 1) + min ;
+    }
+
+    public String gerarQR(int valor, Contas contas)
+    {
+        int cod = getRandomNumberInRange(1000, 9999);
+        String s = contas.getIdConta() + ";" + contas.getNome() + ";" + valor + ";" + cod;
+        System.out.println(s);
+        return s;
+    }
+
+    public boolean pagamento(String QR, )
+    {
+
+    }
 }
