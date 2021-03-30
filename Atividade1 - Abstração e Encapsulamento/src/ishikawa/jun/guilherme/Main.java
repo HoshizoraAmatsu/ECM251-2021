9fe1209a2627ac239c4b7ea3cf3a6071a0738c73
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-30 10:02:15 -0300

    Ajustes
    Deletei o Sistema.java pois não irei usá-lo, dado que apenas os objetos Transacoes, Usuarios e Contas foram pedidos para a criação de geração de QRcode e pagamentos, todos os testes para os objetos serão feitos pela main.java.
    
    Criação de 3 contas na main e teste do método gerarQR

--- Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
+++ Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
     public static void main(String[] args)
     {
-	// write your code here
+	    Contas user1 = new Contas(1, 1000, "user1");
+        Contas user2 = new Contas(2, 250, "user2");
+        Contas user3 = new Contas(3, 3000, "user3");
+
+        Transacoes op = new Transacoes();
+
+        op.gerarQR(250, user1);
     }
 }
--- Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/SIstema.java
+++
@@ -1,7 +0,0 @@
-//Guilherme Jun Ishikawa 18.00906-9
-
-package ishikawa.jun.guilherme;
-
-public class SIstema
-{
-}
