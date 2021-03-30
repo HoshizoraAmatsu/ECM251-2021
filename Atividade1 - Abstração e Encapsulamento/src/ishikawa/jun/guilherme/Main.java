5e792da155b4f730a5586a3fb957b43c2f673dbd
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-03-30 10:57:05 -0300

    Main.java
    Exibe na tela o estado das contas antes e depois das operações.
    
    Operações teste conforme foi pedido pelo cliente(enunciado)

--- Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
+++ Atividade1 - Abstração e Encapsulamento/src/ishikawa/jun/guilherme/Main.java
@@ -12,6 +12,26 @@ public class Main {
 
         Transacoes op = new Transacoes();
 
-        op.gerarQR(250, user1);
+        System.out.println("Estado Inicial:");
+        System.out.println("Nome Usuário: " + user1.getNome() + " - Saldo: " + user1.getSaldo());
+        System.out.println("Nome Usuário: " + user2.getNome() + " - Saldo: " + user2.getSaldo());
+        System.out.println("Nome Usuário: " + user3.getNome() + " - Saldo: " + user3.getSaldo());
+
+        String QR = op.gerarQR(250, user1);
+        System.out.println(QR);
+
+        op.pagamento(QR, user2);
+        op.pagamento(QR, user3);
+        op.pagamento(QR, user2);
+
+        String QR2 = op.gerarQR(1000, user2);
+        System.out.println(QR2);
+
+        op.pagamento(QR2, user2);
+
+        System.out.println("Estado Final:");
+        System.out.println("Nome Usuário: " + user1.getNome() + " - Saldo: " + user1.getSaldo());
+        System.out.println("Nome Usuário: " + user2.getNome() + " - Saldo: " + user2.getSaldo());
+        System.out.println("Nome Usuário: " + user3.getNome() + " - Saldo: " + user3.getSaldo());
     }
 }
