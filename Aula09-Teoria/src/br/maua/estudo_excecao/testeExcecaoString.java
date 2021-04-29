64b59edbbc2cbfd0645c7c59b75f66f6d732e263
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-04-29 13:13:53 -0300

    Aula 09

---
+++ Aula09-Teoria/src/br/maua/estudo_excecao/testeExcecaoString.java
@@ -0,0 +1,22 @@
+package br.maua.estudo_excecao;
+
+public class testeExcecaoString
+{
+    public static void main(String[] args)
+    {
+        String frase = null;
+        String novaFrase = null;
+        try
+        {
+            novaFrase = frase.toUpperCase();
+        }
+        catch (NullPointerException exception)
+        {
+            System.out.println("Uma exceção aconteceu: " + exception.getMessage());
+            frase = "";
+            novaFrase = "";
+        }
+        System.out.println("Frase Antiga: " + frase);
+        System.out.println("Frse Nova: " + novaFrase);
+    }
+}
