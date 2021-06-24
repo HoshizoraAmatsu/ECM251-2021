bdd5083bdb8d0c7f8ab69d4f7d6cee2f4709e2b5
HoshizoraAmatsu <79452593+HoshizoraAmatsu@users.noreply.github.com>
2021-06-24 19:41:45 -0300

    Changes on Member class
    Adicao de metodos e implementacao da interface na classe Member.
    
    Metodos adicionados: Constructor e mudança de hora normal para extra e vice versa
    
    Postar mensagem para cada membro (função e hora)

--- Trabalho-02/MaskSociety/src/ishikawa/jun/guilherme/member/Member.java
+++ Trabalho-02/MaskSociety/src/ishikawa/jun/guilherme/member/Member.java
@@ -1,10 +1,77 @@
 package ishikawa.jun.guilherme.member;
 
-import ishikawa.jun.guilherme.system.PostarMensagem;
+import ishikawa.jun.guilherme.member.types.SystemTime;
 
 public class Member implements PostarMensagem
 {
     protected String username;
     protected String email;
     protected String function;
+    protected SystemTime st = SystemTime.NORMAL;
+
+    public Member(String username, String email, String function) {
+        this.username = username;
+        this.email = email;
+        this.function = function;
+    }
+
+    public void changeST()
+    {
+        if (st == SystemTime.NORMAL)
+            st = SystemTime.EXTRA;
+        else
+            st = SystemTime.NORMAL;
+    }
+
+    @Override
+    public void postarMensagem()
+    {
+        switch (function)
+        {
+            case "MOBILE_MEMBERS":
+                if (st == SystemTime.NORMAL)
+                    System.out.println("Happy Coding!");
+                else
+                    System.out.println("MAsK_S0C13ty");
+                break;
+            case "HEAVY_LIFTERS":
+                if (st == SystemTime.NORMAL)
+                    System.out.println("Podem contar conosco!");
+                else
+                    System.out.println("N00b_qu3_n_Se_r3pita.bat");
+                break;
+            case "BIG_BROTHERS":
+                if (st == SystemTime.NORMAL)
+                    System.out.println("*");
+                else
+                    System.out.println("QU3Ro_S3us_r3curs0s.py");
+                break;
+            case "SCRIPT_GUYS":
+                if (st == SystemTime.NORMAL)
+                    System.out.println("Sempre ajudando as pessoas membros ou não S2!");
+                else
+                    System.out.println("...");
+                break;
+            default:
+                System.out.println(
+                        """
+                        Something went wrong, so have this instead!
+                        
+                        ⣿⡇⣿⣿⣿⠛⠁⣴⣿⡿⠿⠧⠹⠿⠘⣿⣿⣿⡇⢸⡻⣿⣿⣿⣿⣿⣿⣿
+                        ⢹⡇⣿⣿⣿⠄⣞⣯⣷⣾⣿⣿⣧⡹⡆⡀⠉⢹⡌⠐⢿⣿⣿⣿⡞⣿⣿⣿
+                        ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿
+                        ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿
+                        ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮
+                        ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀
+                        ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸
+                        ⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀
+                        ⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾
+                        ⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿
+                        ⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿
+                        ⠄⠄⠄⣿⡇⢬⡻⡇⡄⠄⠄⠄⡰⢖⠔⠉⠄⠄⠄⠄⣼⠏⠄⠄⢸⣿⣿⣿
+                        ⠄⠄⠄⣿⡇⠄⠙⢌⢷⣆⡀⡾⡣⠃⠄⠄⠄⠄⠄⣼⡟⠄⠄⠄⠄⢿⣿⣿
+                        """);
+                break;
+        }
+    }
 }
